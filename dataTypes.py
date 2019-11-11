a397eb6b5ad3b5e24f04c62a65d8d7b1bfcdc725
Punkrockechidna <dan.kaplan@ieleadership.org>
2019-11-10 21:31:30 -0500

    2 new maths functions

--- dataTypes.py
+++ dataTypes.py
@@ -1,6 +1,20 @@
 # Fundamental Data Types
-int
-float
+# int
+print(type(2+4))
+print(2-4)
+print(2*4)
+print(2/4)
+# float
+# Floating point is base2 scientific notation.  1/3 +1/3 + 1/3 != 1
+# https://docs.python.org/3/tutorial/floatingpoint.html
+
+print(2 ** 2)  # ** is to the power of
+print(5 // 4)  # // is how many times it goes in a division rounded down
+print(6 % 4)  # % this is still modulus, what is the remainder
+print(round(3.1))
+print(abs(-10))
+# Math functions https://www.programiz.com/python-programming/modules/math
+
 bool
 str
 list
