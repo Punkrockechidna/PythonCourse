668e0d0e4c042dc0d3e8748bf62e9241ab3dea78
Punkrockechidna <dan.kaplan@ieleadership.org>
2019-11-10 21:46:28 -0500

    best practice

--- dataTypes.py
+++ dataTypes.py
@@ -15,6 +15,21 @@ print(round(3.1))
 print(abs(-10))
 # Math functions https://www.programiz.com/python-programming/modules/math
 
+# Operator precedence Should be pemdas () ** * / + -
+
+complex  # type of number datatype used for complex math
+print(bin(5))  # returns the binary representation
+print(int('0b101', 2))  # converts to base 2 then base 10
+
+# ******************************************************************************************************************
+# Python best practices for variables
+# snake_case
+# start with lowercase or underscore
+# letters, numbers, underscores
+# case sensitive
+# don't overwrite keywords
+# keywords https://www.w3schools.com/python/python_ref_keywords.asp
+
 bool
 str
 list
