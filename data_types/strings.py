30c88bc0f4c803da1e4e327c1fa8c89913ca4ec2
Punkrockechidna <dan.kaplan@ieleadership.org>
2019-11-22 02:56:30 -0500

    finished up to lecture 33

--- data_types/strings.py
+++ data_types/strings.py
@@ -14,3 +14,25 @@ print(c)
 
 # escape sequence whatever comes directly after \ is treated as a string, to use \ in the string use \\
 # \t adds a tab, \n is a newline
+
+# formatted strings
+name = 'Johnny'
+age = 55
+print('hi' + name + '. You are ' + str(age) + ' years old')
+# ORRRRRR use formatted string
+print(f'hi {name}. You are {age} years old')
+# Using Python 2 ther is no f in front to format
+print('hi {1}. You are {0} years old'.format(name, age))
+
+# String indexes
+str = '01234567'
+print(str[0])  # will print 0
+print(str[0:8])  # will print 01234567
+# Now using a "stepover"
+print(str[0:8:2])  # will print 0246
+# In Python the - negative index starts from the end
+print(str[-1])  # will print 7, -2 will print 6
+print(str[::-1])  # will print 76543210
+
+str[start:stop:stepover]  # this is called SLICING
+# strings are immutable, as in strings have immutability.  A string can be reassigned, but an INDEX of a string can NOT change.  Reassign all of none of it
